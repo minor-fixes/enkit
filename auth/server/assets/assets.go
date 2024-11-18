f1c9d58d9e7cdeacb5be272cdfc3e414f1947a90
Scott Minor <minor@minorhacks.com>
2024-11-18 13:03:49 -0500

    go: WIP to eliminate go_embed_data

--- auth/server/assets/assets.go
+++ auth/server/assets/assets.go
@@ -1,3 +1,10 @@
 // Package assets is empty here because this package contains only embedded
 // assets; having a source file here helps `go get -u ./...` to not error.
 package assets
+
+import (
+	"embed"
+)
+
+//go:embed assets
+var AssetFS embed.FS
